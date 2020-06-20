ad3e4f940bf285e5487951948dcebed7d06af395
Caroline Uchôa <carol_uchoa8@hotmail.com>
2020-06-20 17:46:02 -0300

    Exlcuindo build

--- build/static/VRr9x2g-b4HvhhIleZ_xG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/create":[a],"/index":[a],"/p/[id]":[a,"static\u002Fchunks\u002F6ddaa2fa4c227f1ad1120d12382b006bd12b158f.c8706180d9a71951a7f7.js"]}}("static\u002Fchunks\u002Fcd8b12806f9ba0febda8e065211b16d41b5892e2.6a4118a4b5874cc70011.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
