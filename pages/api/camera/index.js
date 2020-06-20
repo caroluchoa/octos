a94d63b9b8aa2cb8db84d941e2d906c44326eafc
Caroline Uchôa <carol_uchoa8@hotmail.com>
2020-06-20 20:22:34 -0300

    Alterando validação no backen

--- pages/api/camera/index.js
+++ pages/api/camera/index.js
@@ -3,11 +3,16 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient() //instancia o prismaclient
 const MANUFACTURERS = ['Secure Câmeras Inc', 'Surveillance Cams LLC', 'DigiEye Group', 'CâmeraFi Inc', 'VidMasters Inc']
 
-
 // Endpoint que recebe a requisição de criação de nova câmera
-
 export default async function handle(req, res) {
   const { cameraname, serialnumber, manufacturer } = req.body
+
+  if (cameraname.length > 50 || serialnumber.length > 16 || !serialnumber.match(/[A-Z0-9]/) || !MANUFACTURERS.includes(manufacturer)) {
+    throw new Error(
+      `Entrada incorreta`
+    )
+  }
+
   const result = await prisma.cameras.create({
     data: {
       cameraname: cameraname,
@@ -16,11 +21,5 @@ export default async function handle(req, res) {
     },
   })
 
-  if (cameraname.length < 50 && serialnumber.length < 16 && serialnumber.match(/[A-Z0-9]/) && MANUFACTURERS.includes(manufacturer)) {
-    res.json(result)
-  } else {
-    throw new Error(
-      `Entrada incorreta`
-    )
-  }
+  res.json(result)
 }
