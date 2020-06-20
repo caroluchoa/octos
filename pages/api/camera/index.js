c228593dd65a8396b497250eaaf3a716c37b31c5
Caroline Uchôa <carol_uchoa8@hotmail.com>
2020-06-20 20:50:49 -0300

    Valida método da requisição

--- pages/api/camera/index.js
+++ pages/api/camera/index.js
@@ -1,16 +1,22 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient() //instancia o prismaclient
+const prisma = new PrismaClient() // instancia o prismaclient
 const MANUFACTURERS = ['Secure Câmeras Inc', 'Surveillance Cams LLC', 'DigiEye Group', 'CâmeraFi Inc', 'VidMasters Inc']
 
 // Endpoint que recebe a requisição de criação de nova câmera
 export default async function handle(req, res) {
-  const { cameraname, serialnumber, manufacturer } = req.body
+  if (req.method !== 'POST') {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`
+    )
+  }
+
+  const { cameraname = '', serialnumber = '', manufacturer = '' } = req.body
 
   if (cameraname.length > 50 || serialnumber.length > 16 || !serialnumber.match(/[A-Z0-9]/) || !MANUFACTURERS.includes(manufacturer)) {
-    throw new Error(
-      `Entrada incorreta`
-    )
+    res.status(400)
+    res.end()
+    return
   }
 
   const result = await prisma.cameras.create({
